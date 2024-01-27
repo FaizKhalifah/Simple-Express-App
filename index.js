191aa55b6996b7ac87fee520064644e7769c11ea
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-27 17:00:27 +0700

    Menambah rute folder

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ import { promises as fsPromises } from 'fs';
 import process from "process";
 import readlinePromises from "readline/promises";
 import express from 'express';
+import path from "path";
 
 const input = readlinePromises.createInterface({
     input:process.stdin,
     output:process.stdout
 })
 
+const currentDirectory = process.cwd();
+
 const app = express();
 
+app.listen(3000);
+
+app.get('/',(req,res)=>{
+    res.sendFile('./Files/main.html', { root: currentDirectory });
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile('./Files/about.html',{ root: currentDirectory});
+})
+
+app.get('/contact',(req,res)=>{
+    res.sendFile('./Files/contact.html',{ root: currentDirectory });
+})
+
+app.use((req,res)=>{
+    res.status(400).sendFile('./Files/404.html',{ root: currentDirectory })
+})
